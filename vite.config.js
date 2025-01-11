a5615c68b58c6dc57946c35d98d572142e217c5e
Hanaa980 <120268876+Hanaa980@users.noreply.github.com>
2025-01-11 21:27:29 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/React-project1/"
+  base:"/React-Project-Routing/"
 })
